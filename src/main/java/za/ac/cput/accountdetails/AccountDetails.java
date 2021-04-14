4712f11aa42898477a9e85229882490dbd14f102
Jasey-max <Jasonblankenberg9242@gmail.com>
2021-04-14 23:36:38 +0200

    Uploading this project on GitHub repository

---
+++ src/main/java/za/ac/cput/accountdetails/AccountDetails.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.accountdetails;
+
+/**
+ *
+ * @author CPUT
+ */
+public class AccountDetails {
+    
+    private String name;
+    private String surname;
+    private int accNum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAccNum() {
+        return accNum;
+    }
+
+    public void setAccNum(int accNum) {
+        this.accNum = accNum;
+    }
+    
+    
+}
